81ba7f0da11fc1eb3afc33ebaaa8f6e7ed17d314
ritik <ritikbaba28@gmail.com>
2022-06-02 23:16:23 +0530

    Material Ui added

---
+++ src/components.js/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+function Test() {
+  return (
+    <div>
+        <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default Test
