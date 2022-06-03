205d4d668e6f07b59e8eb650b0d6d5a09195b9d9
ritik <ritikbaba28@gmail.com>
2022-06-03 18:09:48 +0530

    Navbar added created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
-import Test from './components.js/test';
+// import Test from './components.js/test';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="contained">Hello World</Button>
-      <Test/>
-    </div>
+    // <div className="App">
+    //   <Button variant="contained">Hello World</Button>
+    //   <Test/>
+    // </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
