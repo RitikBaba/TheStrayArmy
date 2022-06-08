96777e0a43c036e0da04d56c217af99c01525fad
ritik <ritikbaba28@gmail.com>
2022-06-08 16:23:12 +0530

    Homepge ready

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,11 +3,15 @@ import { Box } from '@mui/system'
 import React from 'react'
 import cover from './images/cover.jpg'
 import VolunteerActivismTwoToneIcon from '@mui/icons-material/VolunteerActivismTwoTone';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 function Homepage() {
   return (
     <>
-        <Box sx={{bgcolor:'#F7C85C', display:'flex' }}>
+        {/* <Box sx={{bgcolor:'#F7C85C', display:'flex' }}>
             <Box sx={{display:'flex',justifyContent: 'flex-end',mr:35}}>
                 <img src={cover} alt="img not found" height={"450px"} />
             </Box>
@@ -19,7 +23,30 @@ function Homepage() {
                   Donate
                 </Button>
                 </Box>
+        </Box> */}
+        <Card sx={{ display: 'flex',bgcolor:'#F7C85C', alignItems: 'center', justifyContent:'space-between',boxShadow: 'none'}}>
+        <CardMedia
+            component="img"
+            sx={{ height: 500,flex:1 }}
+            image= {cover}
+            alt="Live from space album cover"
+        />
+      <Box sx={{ display: 'flex', flexDirection: 'column',flex:1, alignItems: 'center' }}>
+        <CardContent >
+          <Typography component="div" variant="h3">
+            Live From Space
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            Mac Miller
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+            <Button variant="contained" color="error" sx={{fontSize: 20}}endIcon={<VolunteerActivismTwoToneIcon sx={{ fontSize: 400, alignItems: 'center' }}/>}>
+                Donate
+            </Button>
         </Box>
+      </Box>
+    </Card>
     </>
   )
 }
